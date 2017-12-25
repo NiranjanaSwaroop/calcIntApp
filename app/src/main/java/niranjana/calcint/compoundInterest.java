839fb0155fb33daaa348d851ae550aacd235ce12
NiranjanaSwaroop <niranjan.swaroop@gmail.com>
2017-12-25 14:47:20 +0530

    Calculate Interest

--- app/src/main/java/niranjana/calcint/compoundInterest.java
+++ app/src/main/java/niranjana/calcint/compoundInterest.java
@@ -2,6 +2,7 @@ package niranjana.calcint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class compoundInterest extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class compoundInterest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compound_interest);
+    }
+    public void calcCi(View view){
+        System.out.println("The compound interest is !");
     }
 }
